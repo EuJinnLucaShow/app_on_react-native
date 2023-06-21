90bad692bd6637337568338561236da8aee984dc
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-06-21 12:53:28 +0300

    Модуль-1 Підготовка до роботи виконана

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Модуль-1 Підготовка до роботи виконана 😁</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#85ed93',
     alignItems: 'center',
     justifyContent: 'center',
   },
