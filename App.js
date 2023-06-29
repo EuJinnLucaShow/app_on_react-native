b72f826fef19ab4778fbbfb812f8e55886d16a55
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-06-29 06:08:43 +0300

    Platform.select

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Модуль-1 Підготовка до роботи виконана 😁!</Text>
+      <Text style={styles.title}>Підготовка до роботи виконана 😁!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  height: Platform.OS === 'ios' ? 200 : 100,
   container: {
     flex: 1,
-    backgroundColor: '#85ed93',
+    ...Platform.select({
+      ios: {
+        backgroundColor: 'red',
+      },
+      android: {
+        backgroundColor: 'green',
+      },
+      default: {
+        // other platforms, web for example
+        backgroundColor: 'blue',
+      },
+    }),
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 24,
   },
+  title: {
+    marginTop: 16,
+    paddingVertical: 8,
+    borderWidth: 4,
+    borderColor: "#20232a",
+    borderRadius: 6,
+    backgroundColor: "#61dafb",
+    color: "#20232a",
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "bold"
+  }
 });
