8d0ce5877b9d852e9e9120a664c3c2a18f769810
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-07-07 23:13:31 +0300

    Update Home.jsx

--- Screens/Home.jsx
+++ Screens/Home.jsx
@@ -1,90 +1,61 @@
-import { Text, StyleSheet, TouchableOpacity } from 'react-native';
-import React from 'react';
-import { useNavigation } from '@react-navigation/native';
-import { AntDesign, SimpleLineIcons, Feather } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import PostsScreen from './PostsScreen';
-import CreatePostsScreen from './CreatePostsScreen';
-import ProfileScreen from './ProfileScreen';
+// Home.js
 
-const BottomTabs = createBottomTabNavigator();
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
+function Settings() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+function Profile() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Profile!</Text>
+    </View>
+  );
+}
+
+const Tabs = createBottomTabNavigator();
 
 const Home = () => {
-  const navigation = useNavigation();
   return (
-    <BottomTabs.Navigator
-      initialRouteName="Posts"
-      screenOptions={{
-        tabBarShowLabel: false,
-        tabBarStyle: { height: 80 },
+    <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Profile') {
+            iconName = focused
+              ? 'ios-information-circle'
+              : 'ios-information-circle-outline';
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'ios-list-circle' : 'ios-list';
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
       }}
     >
-      <BottomTabs.Screen
-        options={{
-          tabBarIcon: ({ focused, size, color }) => {
-            return <SimpleLineIcons name="grid" size={20} color={color} />;
-          },
-          headerTitleAlign: 'center',
-          headerRightContainerStyle: { paddingRight: 20 },
-          headerRight: () => (
-            <TouchableOpacity
-              style={styles.logoutButton}
-              activeOpacity={0.5}
-              onPress={() => navigation.navigate('Login')}
-            >
-              <Feather name="log-out" size={24} color="gray" />
-            </TouchableOpacity>
-          ),
-        }}
-        name="PostsScreen"
-        component={PostsScreen}
-      />
-      <BottomTabs.Screen
-        options={{
-          tabBarIcon: () => {
-            return (
-              <TouchableOpacity
-                style={styles.addButton}
-                activeOpacity={0.5}
-                onPress={() => navigation.navigate('CreatePostsScreen')}
-              >
-                <Text style={styles.addButtonText}>+</Text>
-              </TouchableOpacity>
-            );
-          },
-          headerShown: false,
-          tabBarStyle: { display: 'none' },
-          headerTitleAlign: 'center',
-        }}
-        name="CreatePostsScreen"
-        component={CreatePostsScreen}
-      />
-      <BottomTabs.Screen
-        options={{
-          tabBarIcon: ({ focused, size, color }) => {
-            return <AntDesign name="user" size={20} color={color} />;
-          },
-          headerShown: false,
-        }}
-        name="ProfileScreen"
-        component={ProfileScreen}
-      />
-    </BottomTabs.Navigator>
+      <Tabs.Screen name="Settings" component={Settings} />
+      <Tabs.Screen name="Profile" component={Profile} />
+    </Tabs.Navigator>
   );
 };
 
 const styles = StyleSheet.create({
-  addButton: {
-    backgroundColor: '#FF6C00',
-    height: 40,
-    width: 70,
-    justifyContent: 'center',
+  container: {
+    flex: 1,
     alignItems: 'center',
-    borderRadius: 20,
-  },
-  addButtonText: {
-    color: '#ffffff',
-    fontSize: 18,
+    justifyContent: 'center',
   },
 });
 
